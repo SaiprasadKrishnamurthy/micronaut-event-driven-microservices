467826be553dc1c3510f16be5a82d013e8d8f6e0
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2021-09-25 21:53:02 +0530

    Greeting Event Added

--- mn2/src/main/kotlin/com/sai/model/Events.kt
+++ mn2/src/main/kotlin/com/sai/model/Events.kt
@@ -2,3 +2,6 @@ package com.sai.model
 
 data class HelloEvent(val message: String, override val eventId: String, override val userId: String) :
     BaseEvent(eventId, userId)
+
+data class GreetingEvent(val message: String, override val eventId: String, override val userId: String) :
+    BaseEvent(eventId, userId)
--- mn2/src/main/kotlin/com/sai/model/Payloads.kt
+++
@@ -1,3 +0,0 @@
-package com.sai.model
-
-data class Hello(val message: String, override val eventId: String, override val userId: String) : BaseModel(eventId, userId)
